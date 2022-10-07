d79d70114bf4b89a0a3fb826f5667356971bcf7a
lohamedevelop <lohamedevelop@gmail.com>
2022-10-06 23:39:33 -0300

    tela clima detalhes e navigation

--- previsao_clima/lib/views/climaDetalhesPage.dart
+++ previsao_clima/lib/views/climaDetalhesPage.dart
@@ -17,7 +17,32 @@ class _ClimaDetalhesPage extends State<ClimaDetalhesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.clima.cidade),
+        title: Text(widget.clima.cidade + ' ' + widget.clima.data),
+      ),
+      body: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    child: Image.asset(widget.clima.icon),
+                    width: 60,
+                  ),
+                  Container(width: 10),
+                  Text(
+                    widget.clima.descricao,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: -1,
+                        color: Colors.green),
+                  )
+                ],
+              )),
+        ],
       ),
     );
   }
